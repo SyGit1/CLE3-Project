ce13d58211044610d6983123ad2e3329c0000cd3
ward <1060834@hr.nl>
2023-03-13 14:51:50 +0100

    1

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <img src="./img/logo.png">
+    </header>
+
+    <main>
+        <div id="about">
+            <h2>Onze missie</h2>
+            <p>Zorg voor motorisch beperkten toegankelijker maken.</p>
+        </div>
+        <div id="categories">
+            <img src="./img/card1.jpeg">
+            <img src="./img/card2.jpeg">
+            <img src="./img/card3.jpeg">
+            <img src="./img/card4.jpeg">
+            <img src="./img/card5.jpeg">
+        </div>
+    </main>
+</body>
+</html>
