e7889e0b9c17a342bcd6eb800df39e0bb63d7304
Rayel2002 <1002893@hr.nl>
2023-03-15 10:58:22 +0100

    index toegevoegd

--- maps.php
+++ maps.php
@@ -8,6 +8,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="js/maps.js" defer></script>
+    <script async src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
     <title>Document</title>
 </head>
 <body>
