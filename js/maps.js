7ca2b7e41a090f99383f20370e6efa3f029de988
Rayel2002 <1002893@hr.nl>
2023-03-27 15:37:16 +0200

    maps api begonnen

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,15 @@
-let map;
+let platform = new H.service.Platform({
+    'apikey': '9iw8-Sw_KHN2NFYkF6Bwkg3-opWgHgr3oCfbv_m-l4Q'
+});
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+// Obtain the default map types from the platform object:
+let defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+let map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+        zoom: 10,
+        center: { lat: 52.5, lng: 13.4 }
     });
-}
-
-window.initMap = initMap;
--- maps.php
+++ maps.php
@@ -8,10 +8,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="js/maps.js" defer></script>
-    <script async src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js"
+            type="text/javascript" charset="utf-8"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js"
+            type="text/javascript" charset="utf-8"></script>
     <title>Document</title>
 </head>
 <body>
+<div style="width: 640px; height: 480px" id="mapContainer"></div>
 
 </body>
 </html>
+
