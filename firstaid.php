51d961abb1ff873a589394a00c4e2d10c6ae226d
ward <1060834@hr.nl>
2023-03-27 15:33:08 +0200

    homepage

---
+++ firstaid.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./css/firstaid.css">
+    <title>Huisartsen in de buurt</title>
+</head>
+
+<body>
+<header>
+    <a href="index.html">
+        <img src="./img/firstaid.png">
+    </a>
+    <a href="review.php">Sign Up</a>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+    <a href="index.html" class="fa fa-arrow-left"></a>
+</footer>
+</body>
+
+</html>
