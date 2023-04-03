93c597c1a5dc3cb1ac507c2935c1e4aed559181e
Rayel2002 <1002893@hr.nl>
2023-04-03 14:41:19 +0200

    Werkende Maps api

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,168 @@
-let platform = new H.service.Platform({
+function landmarkGeocode(platform) {
+    const geocoder = platform.getSearchService(),
+        landmarkGeocodingParameters = {
+            q: 'Erasmus MC',
+            at: '0,0',
+            limit: 1
+        };
+
+    geocoder.discover(
+        landmarkGeocodingParameters,
+        onSuccess,
+        onError
+    );
+}
+
+/**
+ * This function will be called once the Geocoder REST API provides a response
+ * @param  {Object} result          A JSONP object representing the  location(s) found.
+ *
+ * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-type-response-geocode.html
+ */
+function onSuccess(result) {
+    const locations = result.items;
+    /*
+     * The styling of the geocoding response on the map is entirely under the developer's control.
+     * A representitive styling can be found the full JS + HTML code of this example
+     * in the functions below:
+     */
+    addLocationsToMap(locations);
+    addLocationsToPanel(locations);
+    // ... etc.
+}
+
+/**
+ * This function will be called if a communication error occurs during the JSON-P request
+ * @param  {Object} error  The error message received.
+ */
+function onError(error) {
+    alert('Can\'t reach the remote server');
+}
+
+/**
+ * Boilerplate map initialization code starts below:
+ */
+
+//Step 1: initialize communication with the platform
+// In your own code, replace variable window.apikey with your own apikey
+var platform = new H.service.Platform({
     'apikey': '9iw8-Sw_KHN2NFYkF6Bwkg3-opWgHgr3oCfbv_m-l4Q'
 });
+const defaultLayers = platform.createDefaultLayers();
 
-// Obtain the default map types from the platform object:
-let defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-let map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-        zoom: 10,
-        center: { lat: 52.5, lng: 13.4 }
+//Step 2: initialize a map - this map is centered over Rotterdam
+const map = new H.Map(document.getElementById('map'),
+    defaultLayers.vector.normal.map, {
+        center: {lat: 51.9173828, lng: 4.4846457},
+        zoom: 15,
+        pixelRatio: window.devicePixelRatio || 1
     });
+// add a resize listener to make sure that the map occupies the whole container
+window.addEventListener('resize', () => map.getViewPort().resize());
+
+const locationsContainer = document.getElementById('panel');
+
+//Step 3: make the map interactive
+// MapEvents enables the event system
+// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Create the default UI components
+const ui = H.ui.UI.createDefault(map, defaultLayers);
+
+// Hold a reference to any infobubble opened
+let bubble;
+
+/**
+ * Opens/Closes a infobubble
+ * @param  {H.geo.Point} position     The location on the map.
+ * @param  {String} text              The contents of the infobubble.
+ */
+function openBubble(position, text){
+    if(!bubble){
+        bubble =  new H.ui.InfoBubble(
+            position,
+            {content: text});
+        ui.addBubble(bubble);
+    } else {
+        bubble.setPosition(position);
+        bubble.setContent(text);
+        bubble.open();
+    }
+}
+
+/**
+ * Creates a series of list items for each location found, and adds it to the panel.
+ * @param {Object[]} locations An array of locations as received from the
+ *                             H.service.GeocodingService
+ */
+function addLocationsToPanel(locations){
+
+    const nodeOL = document.createElement('ul');
+    let i;
+
+    nodeOL.style.fontSize = 'small';
+    nodeOL.style.marginLeft ='5%';
+    nodeOL.style.marginRight ='5%';
+
+
+    for (i = 0;  i < locations.length; i += 1) {
+        let location = locations[i],
+            li = document.createElement('li'),
+            divLabel = document.createElement('div'),
+            content =  location.title  + '</br>';
+        position = location.position;
+
+        content += '<strong>Huisnummer:</strong> ' + location.address.houseNumber + '<br/>';
+        content += '<strong>Straat:</strong> '  + location.address.label + '<br/>';
+        content += '<strong>Stad:</strong> ' + location.address.city + '<br/>';
+        content += '<strong>Postcode:</strong> ' + location.address.postalCode + '<br/>';
+        content += '<strong>Provincie:</strong> ' + location.address.county + '<br/>';
+        content += '<strong>Land:</strong> ' + location.address.countryName + '<br/>';
+        content += 'position: ' +
+            Math.abs(position.lat.toFixed(4)) + ((position.lat > 0) ? 'N' : 'S') +
+            ' ' + Math.abs(position.lng.toFixed(4)) + ((position.lng > 0) ? 'E' : 'W') + '<br/>';
+
+        divLabel.innerHTML = content;
+        li.appendChild(divLabel);
+
+        nodeOL.appendChild(li);
+    }
+
+    locationsContainer.appendChild(nodeOL);
+}
+
+
+/**
+ * Creates a series of H.map.Markers for each location found, and adds it to the map.
+ * @param {Object[]} locations An array of locations as received from the
+ *                             H.service.GeocodingService
+ */
+function addLocationsToMap(locations){
+    const group = new H.map.Group();
+    let i;
+
+    // Add a marker for each location found
+    for (i = 0;  i < locations.length; i += 1) {
+        let location = locations[i];
+        marker = new H.map.Marker(location.position);
+        marker.label = location.title;
+        group.addObject(marker);
+    }
+
+    group.addEventListener('tap', function (evt) {
+        map.setCenter(evt.target.getGeometry());
+        openBubble(
+            evt.target.getGeometry(), evt.target.label);
+    }, false);
+
+    // Add the locations group to the map
+    map.addObject(group);
+    map.getViewModel().setLookAtData({
+        bounds: group.getBoundingBox()
+    });
+    map.setZoom(Math.min(map.getZoom(), 16));
+}
+
+// Now use the map as required...
+landmarkGeocode(platform);
--- maps.php
+++ maps.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,15 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="js/maps.js" defer></script>
-    <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js"
-            type="text/javascript" charset="utf-8"></script>
-    <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js"
-            type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-core.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>
+    <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css"/>
+    <link rel="stylesheet" type="text/css" href="css/maps.css">
     <title>Document</title>
 </head>
-<body>
-<div style="width: 640px; height: 480px" id="mapContainer"></div>
+<main>
+    <body id="geocode">
+    <div id="map"></div>
+    <div id="panel"></div>
+    <script type="text/javascript" src='demo.js'></script>
+    </body>
+</main>
 
-</body>
 </html>
 
