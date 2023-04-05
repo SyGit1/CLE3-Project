0d0a60972b2eec3e51efc3586a59351b2f5b6932
Rayel2002 <1002893@hr.nl>
2023-04-05 10:03:20 +0200

    linkje verplaatst

--- pharmacy.php
+++ pharmacy.php
@@ -11,6 +11,7 @@
     <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>
     <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>
     <script type="text/javascript" src="js/darkMode.js" defer></script>
+    <script type="text/javascript" src='demo.js'></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css"/>
@@ -41,7 +42,6 @@
 <main>
     <div id="map"></div>
     <div id="panel"></div>
-    <script type="text/javascript" src='demo.js'></script>
 </main>
 <footer>
     <a href="index.php" class="fa fa-arrow-left"></a>
