88a5c20924801a579fd0a47aecc178e190ba0218
Rayel2002 <1002893@hr.nl>
2023-04-05 10:00:33 +0200

    maps in alle paginas gezet

--- maps.php
+++ maps.php
@@ -10,29 +10,34 @@
     <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>
     <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>
     <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>
+    <script type="text/javascript" src="js/darkMode.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css"/>
     <link rel="stylesheet" type="text/css" href="css/maps.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Document</title>
 </head>
-
 <body id="geocode">
 <header>
     <a href="index.php">
         <img src="./img/logo.png">
     </a>
     <div>
-        <a href="sign-up.php">Sign Up</a>
-        <button id="mybtn" onclick="darkmode()">Dark</button>
+        <button id="mybtn" onclick="darkmode()">
+            <i class="fa-solid fa-moon"></i>
+        </button>
     </div>
 </header>
+
     <main>
     <div id="map"></div>
     <div id="panel"></div>
     <script type="text/javascript" src='demo.js'></script>
 </main>
+<footer>
+    <a href="index.php" class="fa fa-arrow-left"></a>
+</footer>
 </body>
-
-
 </html>
 
--- physio.php
+++ physio.php
@@ -2,19 +2,24 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="./css/physio.css">
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script type="text/javascript" src="js/maps.js" defer></script>
+        <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-core.js"></script>
+        <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-service.js"></script>
+        <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-ui.js"></script>
+        <script type="text/javascript" src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js"></script>
+        <script type="text/javascript" src="js/darkMode.js" defer></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css"/>
+        <link rel="stylesheet" type="text/css" href="css/maps.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Fysiotherapeuten in de buurt</title>
 </head>
-
-<body>
-
-<script src="./js/darkMode.js"></script>
-
+<body id="geocode">
 <header>
     <a href="index.php">
         <img src="./img/physio.png">
@@ -26,11 +31,11 @@
         </button>
     </div>
 </header>
-
 <main>
-
+    <div id="map"></div>
+    <div id="panel"></div>
+    <script type="text/javascript" src='demo.js'></script>
 </main>
-
 <footer>
     <a href="index.php" class="fa fa-arrow-left"></a>
 </footer>
